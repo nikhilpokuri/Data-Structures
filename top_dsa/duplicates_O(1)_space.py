45732ad6e265145aaf5fd351eb5daabbdf9c9ca3
nikhil <nikhilpokuri143@gmail.com>
2024-02-23 10:31:03 +0530

    daily dsa

--- top_dsa/duplicates_O(1)_space.py
+++ top_dsa/duplicates_O(1)_space.py
@@ -1,17 +1,17 @@
+''' THIS METHOD WILL WORK WHERE arr[i] < len(arr)  '''
 def find_duplicates(arr):
 	duplicates = []
 	n = len(arr)
 	for i in range(n):
 		index = arr[i] % n
-		arr[index] += 10
-		
-	for i in range(n):
-		if arr[i] // 10 >= 2:
-			duplicates.append(i)
+		arr[index] += n
+	for j in range(n):
+		arr[j] //= n
+		if arr[j] >= 2:
+			duplicates.append(j)
 	return duplicates
 
-
-arr = [1,2,1,3,4,3]
+arr = [1,1,2,3,2,4,4,3,4]
 print("The repeating elements are:")
 ans = find_duplicates(arr)
 for x in ans:
