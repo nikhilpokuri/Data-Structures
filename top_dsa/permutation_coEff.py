0fb72390c3254f9c43e01ae89cbbc176b8af4cd3
nikhil <nikhilpokuri143@gmail.com>
2024-06-23 15:16:49 +0530

    moved

--- top_dsa/permutation_coEff.py
+++
@@ -1,6 +0,0 @@
-def permutationCoeff(n, k):
-    res = 1
-    for i in range(k):
-        res *= (n-i)
-    return res%((10**9)+7)
-print(permutationCoeff(10,3))
--- top_dsa/reverse_array.py
+++
@@ -1,9 +0,0 @@
-arr = [2,4,-1,5,-9,-4]
-start = 0
-end = len(arr)-1
-
-while start<end:
-    arr[start],arr[end] = arr[end],arr[start]
-    start += 1
-    end -= 1
-print(arr)
